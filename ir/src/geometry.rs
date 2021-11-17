e2e62a1b2e138fc30e9e590ae74abe18a731a527
Park Joon-Kyu <segfault87@gmail.com>
2021-11-17 21:15:51 +0900

    Add BoundingBox2

--- ir/src/geometry.rs
+++ ir/src/geometry.rs
@@ -1,5 +1,83 @@
-use ldraw::Vector3;
+use ldraw::{Vector2, Vector3};
 use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct BoundingBox2 {
+    pub min: Vector2,
+    pub max: Vector2,
+}
+
+impl BoundingBox2 {
+    pub fn zero() -> Self {
+        BoundingBox2 {
+            min: Vector2::new(0.0, 0.0),
+            max: Vector2::new(0.0, 0.0),
+        }
+    }
+
+    pub fn new(a: &Vector2, b: &Vector2) -> Self {
+        let (min_x, max_x) = if a.x > b.x { (b.x, a.x) } else { (a.x, b.x) };
+        let (min_y, max_y) = if a.y > b.y { (b.y, a.y) } else { (a.y, b.y) };
+
+        BoundingBox2 {
+            min: Vector2::new(min_x, min_y),
+            max: Vector2::new(max_x, max_y),
+        }
+    }
+
+    pub fn len_x(&self) -> f32 {
+        self.max.x - self.min.x
+    }
+
+    pub fn len_y(&self) -> f32 {
+        self.max.y - self.min.y
+    }
+
+    pub fn is_null(&self) -> bool {
+        self.min.x == 0.0
+            && self.min.y == 0.0
+            && self.max.x == 0.0
+            && self.max.y == 0.0
+    }
+
+    pub fn update_point(&mut self, v: &Vector2) {
+        if self.is_null() {
+            self.min = *v;
+            self.max = *v;
+        } else {
+            if self.min.x > v.x {
+                self.min.x = v.x;
+            }
+            if self.min.y > v.y {
+                self.min.y = v.y;
+            }
+            if self.max.x < v.x {
+                self.max.x = v.x;
+            }
+            if self.max.y < v.y {
+                self.max.y = v.y;
+            }
+        }
+    }
+
+    pub fn update(&mut self, bb: &BoundingBox2) {
+        self.update_point(&bb.min);
+        self.update_point(&bb.max);
+    }
+
+    pub fn center(&self) -> Vector2 {
+        (self.min + self.max) * 0.5
+    }
+
+    pub fn points(&self) -> [Vector2; 4] {
+        [
+            Vector2::new(self.min.x, self.min.y),
+            Vector2::new(self.min.x, self.max.y),
+            Vector2::new(self.max.x, self.min.y),
+            Vector2::new(self.max.x, self.max.y),
+        ]
+    }
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct BoundingBox3 {
