f8fb82de8e1693c97e6f77fd059de63dbe722db6
Park Joon-Kyu <segfault87@gmail.com>
2021-11-21 20:19:54 +0900

    olr: Fix incorrect clipping and enable antialiasing

--- olr/src/context.rs
+++ olr/src/context.rs
@@ -1,10 +1,7 @@
-use std::{
-    panic::catch_unwind,
-    rc::Rc,
-};
+use std::rc::Rc;
 
 use glow::{
-    Context as GlContext, HasContext
+    Context as GlContext, HasContext, PixelPackData,
 };
 use glutin::{
     dpi::PhysicalSize,
@@ -13,6 +10,9 @@ use glutin::{
     Context, ContextBuilder, CreationError, GlProfile, GlRequest,
     NotCurrent, PossiblyCurrent
 };
+use ldraw::Vector2;
+use ldraw_ir::geometry::BoundingBox2;
+use image::RgbaImage;
 use ldraw_renderer::{
     shader::ProgramManager,
     state::RenderingContext
@@ -27,11 +27,69 @@ pub struct OlrContext {
     pub gl: Rc<GlContext>,
     pub rendering_context: RenderingContext<GlContext>,
     
-    gl_context: Context<PossiblyCurrent>,
+    _gl_context: Context<PossiblyCurrent>,
 
     framebuffer: Option<glow::NativeFramebuffer>,
     renderbuffer_color: Option<glow::NativeRenderbuffer>,
     renderbuffer_depth: Option<glow::NativeRenderbuffer>,
+}
+
+impl OlrContext {
+
+    pub fn get_framebuffer_contents(&self, bounds: Option<BoundingBox2>) -> RgbaImage {
+        let mut pixels: Vec<u8> = Vec::new();
+        pixels.resize(4 * self.width * self.height, 0);
+
+        let gl = &self.gl;
+        unsafe {
+            // Transfer from multisampled fbo to normal fbo
+            let framebuffer_wo_multisample = gl.create_framebuffer().ok();
+            gl.bind_framebuffer(glow::FRAMEBUFFER, framebuffer_wo_multisample);
+            let renderbuffer_color = gl.create_renderbuffer().ok();
+            gl.bind_renderbuffer(glow::RENDERBUFFER, renderbuffer_color);
+            gl.renderbuffer_storage(
+                glow::RENDERBUFFER, glow::RGBA8, self.width as _, self.height as _
+            );
+            gl.framebuffer_renderbuffer(glow::FRAMEBUFFER, glow::COLOR_ATTACHMENT0, glow::RENDERBUFFER, renderbuffer_color);
+
+            gl.bind_framebuffer(glow::READ_FRAMEBUFFER, self.framebuffer);
+            gl.bind_framebuffer(glow::DRAW_FRAMEBUFFER, framebuffer_wo_multisample);
+            gl.blit_framebuffer(
+                0, 0, self.width as _, self.height as _,
+                0, 0, self.width as _, self.height as _,
+                glow::COLOR_BUFFER_BIT,
+                glow::NEAREST
+            );
+            
+            gl.bind_framebuffer(glow::FRAMEBUFFER, framebuffer_wo_multisample);
+            gl.read_buffer(glow::COLOR_ATTACHMENT0);
+            gl.read_pixels(
+                0, 0, self.width as _, self.height as _, glow::RGBA, glow::UNSIGNED_BYTE,
+                PixelPackData::Slice(pixels.as_mut())
+            );
+
+            gl.delete_renderbuffer(renderbuffer_color.unwrap());
+            gl.delete_framebuffer(framebuffer_wo_multisample.unwrap());
+        }
+
+        let bounds = bounds.unwrap_or_else(|| BoundingBox2::new(&Vector2::new(0.0, 0.0), &Vector2::new(1.0, 1.0)));
+
+        let x1 = (bounds.min.x * self.width as f32) as usize;
+        let y1 = (bounds.min.y * self.height as f32) as usize;
+        let x2 = (bounds.max.x * self.width as f32) as usize;
+        let y2 = (bounds.max.y * self.height as f32) as usize;
+        let cw = x2 - x1;
+        let ch = y2 - y1;
+
+        let mut pixels_rearranged: Vec<u8> = Vec::new();
+        for v in (y1..y2).rev() {
+            let s = 4 * v as usize * self.width as usize + x1 * 4;
+            pixels_rearranged.extend_from_slice(&pixels[s..(s + (cw * 4))]);
+        }
+
+        RgbaImage::from_raw(cw as _, ch as _, pixels_rearranged).unwrap()
+    }
+
 }
 
 impl Drop for OlrContext {
@@ -66,20 +124,22 @@ fn create_context(
     let renderbuffer_depth;
     let renderbuffer_color;
     unsafe {
+        gl.enable(glow::MULTISAMPLE);
+
         framebuffer = gl.create_framebuffer().ok();
         gl.bind_framebuffer(glow::FRAMEBUFFER, framebuffer);
 
         renderbuffer_depth = gl.create_renderbuffer().ok();
         gl.bind_renderbuffer(glow::RENDERBUFFER, renderbuffer_depth);
-        gl.renderbuffer_storage(
-            glow::RENDERBUFFER, glow::DEPTH_COMPONENT32F, width as _, height as _
+        gl.renderbuffer_storage_multisample(
+            glow::RENDERBUFFER, 4, glow::DEPTH_COMPONENT32F, width as _, height as _
         );
         gl.framebuffer_renderbuffer(glow::FRAMEBUFFER, glow::DEPTH_ATTACHMENT, glow::RENDERBUFFER, renderbuffer_depth);
 
         renderbuffer_color = gl.create_renderbuffer().ok();
         gl.bind_renderbuffer(glow::RENDERBUFFER, renderbuffer_color);
-        gl.renderbuffer_storage(
-            glow::RENDERBUFFER, glow::RGBA8, width as _, height as _
+        gl.renderbuffer_storage_multisample(
+            glow::RENDERBUFFER, 4, glow::RGBA8, width as _, height as _
         );
         gl.framebuffer_renderbuffer(glow::FRAMEBUFFER, glow::COLOR_ATTACHMENT0, glow::RENDERBUFFER, renderbuffer_color);
     }
@@ -94,7 +154,7 @@ fn create_context(
         gl,
         rendering_context,
 
-        gl_context: context,
+        _gl_context: context,
 
         framebuffer,
         renderbuffer_color,
--- olr/src/ops.rs
+++ olr/src/ops.rs
@@ -1,16 +1,12 @@
-use std::{
-    collections::HashMap,
-    rc::Rc,
-    vec::Vec,
-};
+use std::collections::HashMap;
 
-use glow::{Context as GlContext, HasContext, PixelPackData};
+use cgmath::EuclideanSpace;
+use glow::{Context as GlContext, HasContext};
 use image::RgbaImage;
 use ldraw::{
     color::Material,
-    PartAlias, Point3
+    PartAlias, Point3,
 };
-use ldraw_ir::geometry::BoundingBox2;
 use ldraw_renderer::{
     display_list::DisplayList,
     part::Part,
@@ -21,33 +17,6 @@ use crate::{
     context::OlrContext,
     utils::calculate_bounding_box,
 };
-
-fn buffer_to_image(context: &OlrContext, gl: Rc<GlContext>, bounds: &BoundingBox2) -> RgbaImage {
-    let mut pixels: Vec<u8> = Vec::new();
-    pixels.resize(4 * context.width * context.height, 0);
-    unsafe {
-        gl.read_buffer(glow::COLOR_ATTACHMENT0);
-        gl.read_pixels(
-            0, 0, context.width as _, context.height as _, glow::RGBA, glow::UNSIGNED_BYTE,
-            PixelPackData::Slice(pixels.as_mut())
-        );
-    }
-
-    let x1 = (bounds.min.x * context.width as f32) as usize;
-    let y1 = (bounds.min.y * context.height as f32) as usize;
-    let x2 = (bounds.max.x * context.width as f32) as usize;
-    let y2 = (bounds.max.y * context.height as f32) as usize;
-    let cw = x2 - x1;
-    let ch = y2 - y1;
-
-    let mut pixels_rearranged: Vec<u8> = Vec::new();
-    for v in (y1..y2).rev() {
-        let s = 4 * v as usize * context.width as usize;
-        pixels_rearranged.extend_from_slice(&pixels[s..(s + (cw * 4))]);
-    }
-
-    RgbaImage::from_raw(cw as _, ch as _, pixels_rearranged).unwrap()
-} 
 
 pub fn render_single_part(context: &mut OlrContext, part: &Part<GlContext>, material: &Material) -> RgbaImage {
     let gl = &context.gl;
@@ -67,7 +36,7 @@ pub fn render_single_part(context: &mut OlrContext, part: &Part<GlContext>, mate
         gl.flush();
     }
 
-    buffer_to_image(context, Rc::clone(&gl), &bounds)
+    context.get_framebuffer_contents(Some(bounds))
 }
 
 pub fn render_display_list(
@@ -83,8 +52,8 @@ pub fn render_display_list(
         gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
     }
 
-    let camera = OrthographicCamera::new_isometric(Point3::new(0.0, 0.0, 0.0));
     let bounding_box = calculate_bounding_box(parts, display_list);
+    let camera = OrthographicCamera::new_isometric(Point3::from_vec(bounding_box.center()));
     let bounds = rc.apply_orthographic_camera(&camera, &OrthographicViewBounds::BoundingBox3(bounding_box.clone())).unwrap();
     
     rc.render_display_list(&parts, display_list, false);
@@ -94,5 +63,5 @@ pub fn render_display_list(
         gl.flush();
     }
 
-    buffer_to_image(context, Rc::clone(&gl), &bounds)
+    context.get_framebuffer_contents(Some(bounds))
 }
