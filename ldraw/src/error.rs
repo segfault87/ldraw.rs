34378006f66e60c8fbc76c7f66cf32f0a2ac3412
Park Joon-Kyu <segfault87@gmail.com>
2021-11-28 22:58:23 +0900

    ldraw: implement Error trait on error types

--- ldraw/src/error.rs
+++ ldraw/src/error.rs
@@ -1,5 +1,8 @@
-use std::fmt;
-use std::io::Error as IoError;
+use std::{
+    error::Error,
+    fmt,
+    io::Error as IoError,
+};
 
 #[derive(Debug)]
 pub enum ParseError {
@@ -13,58 +16,9 @@ pub enum ParseError {
     MultipartDocument,
 }
 
-#[derive(Debug)]
-pub struct DocumentParseError {
-    pub line: usize,
-    pub error: ParseError,
-}
-
-#[derive(Debug)]
-pub enum ColorDefinitionParseError {
-    ParseError(ParseError),
-    DocumentParseError(DocumentParseError),
-    UnknownMaterial(String),
-}
-
-#[derive(Debug)]
-pub enum SerializeError {
-    NoSerializable,
-    IoError(Box<IoError>),
-}
-
-#[derive(Debug)]
-pub enum LibraryError {
-    NoLDrawDir,
-    IoError(Box<IoError>),
-}
-
 impl From<IoError> for ParseError {
     fn from(e: IoError) -> ParseError {
         ParseError::IoError(Box::new(e))
-    }
-}
-
-impl From<IoError> for SerializeError {
-    fn from(e: IoError) -> SerializeError {
-        SerializeError::IoError(Box::new(e))
-    }
-}
-
-impl From<IoError> for LibraryError {
-    fn from(e: IoError) -> LibraryError {
-        LibraryError::IoError(Box::new(e))
-    }
-}
-
-impl From<ParseError> for ColorDefinitionParseError {
-    fn from(e: ParseError) -> ColorDefinitionParseError {
-        ColorDefinitionParseError::ParseError(e)
-    }
-}
-
-impl From<DocumentParseError> for ColorDefinitionParseError {
-    fn from(e: DocumentParseError) -> ColorDefinitionParseError {
-        ColorDefinitionParseError::DocumentParseError(e)
     }
 }
 
@@ -85,9 +39,49 @@ impl fmt::Display for ParseError {
     }
 }
 
+impl Error for ParseError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ParseError::IoError(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DocumentParseError {
+    pub line: usize,
+    pub error: ParseError,
+}
+
+impl From<DocumentParseError> for ColorDefinitionParseError {
+    fn from(e: DocumentParseError) -> ColorDefinitionParseError {
+        ColorDefinitionParseError::DocumentParseError(e)
+    }
+}
+
 impl fmt::Display for DocumentParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} (at line {})", self.error, self.line)
+    }
+}
+
+impl Error for DocumentParseError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.error)
+    }
+}
+
+#[derive(Debug)]
+pub enum ColorDefinitionParseError {
+    ParseError(ParseError),
+    DocumentParseError(DocumentParseError),
+    UnknownMaterial(String),
+}
+
+impl From<ParseError> for ColorDefinitionParseError {
+    fn from(e: ParseError) -> ColorDefinitionParseError {
+        ColorDefinitionParseError::ParseError(e)
     }
 }
 
@@ -101,11 +95,72 @@ impl fmt::Display for ColorDefinitionParseError {
     }
 }
 
+impl Error for ColorDefinitionParseError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ColorDefinitionParseError::ParseError(e) => Some(e),
+            ColorDefinitionParseError::DocumentParseError(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum SerializeError {
+    NoSerializable,
+    IoError(Box<IoError>),
+}
+
+impl From<IoError> for SerializeError {
+    fn from(e: IoError) -> SerializeError {
+        SerializeError::IoError(Box::new(e))
+    }
+}
+
+impl fmt::Display for SerializeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SerializeError::NoSerializable => write!(f, "Statement is not serializable."),
+            SerializeError::IoError(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl Error for SerializeError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            SerializeError::IoError(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum LibraryError {
+    NoLDrawDir,
+    IoError(Box<IoError>),
+}
+
+impl From<IoError> for LibraryError {
+    fn from(e: IoError) -> LibraryError {
+        LibraryError::IoError(Box::new(e))
+    }
+}
+
 impl fmt::Display for LibraryError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             LibraryError::NoLDrawDir => write!(f, "No LDraw library found."),
             LibraryError::IoError(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl Error for LibraryError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            LibraryError::IoError(e) => Some(e),
+            _ => None,
         }
     }
 }
