25b75c48692925e22e5ceab2d81b5cc59d4a10bd
Park Joon-Kyu <segfault87@gmail.com>
2022-01-15 12:27:45 +0900

    baker: Refactor to utilize multithreading and async io

--- tools/baker/src/main.rs
+++ tools/baker/src/main.rs
@@ -1,31 +1,40 @@
 use std::{
     env,
-    fs::File,
-    io::{BufReader, BufWriter},
-    path::Path,
     sync::{Arc, RwLock},
 };
 
-use bincode::serialize_into;
+use async_std::{
+    fs::File,
+    io::{BufReader, BufWriter},
+    path::{Path, PathBuf},
+    stream::StreamExt,
+};
+use bincode::serialize;
 use clap::{Arg, App};
+use futures::{
+    AsyncWriteExt,
+    future::join_all,
+};
+use itertools::Itertools;
 use ldraw::{
     color::MaterialRegistry,
     library::{
-        load_files,
-        scan_ldraw_directory,
         CacheCollectionStrategy,
+        LibraryLoader,
         PartCache,
-        PartDirectoryNative,
-        ResolutionMap
+        resolve_dependencies,
     },
+    resolvers::local::LocalLoader,
     parser::{
         parse_color_definition,
         parse_multipart_document
     },
 };
-use ldraw_ir::part::{bake_part};
+use ldraw_ir::part::bake_part;
+use tokio::task::spawn_blocking;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let matches = App::new("baker")
         .about("Postprocess LDraw model files")
         .arg(Arg::with_name("ldraw_dir")
@@ -60,7 +69,7 @@ fn main() {
     let output_path = match matches.value_of("output_path") {
         Some(v) => {
             let path = Path::new(v.clone());
-            if !path.is_dir() {
+            if !path.is_dir().await {
                 panic!("{} is not a proper output directory", v);
             }
             Some(path)
@@ -68,23 +77,25 @@ fn main() {
         None => None,
     };
 
-    let ldrawpath = Path::new(&ldrawdir);
-    let directory = Arc::new(RwLock::new(
-        scan_ldraw_directory(&ldrawdir).expect("Not a LDraw path.")
-    ));
-
+    let ldrawpath = PathBuf::from(&ldrawdir);
+    
     let colors = parse_color_definition(&mut BufReader::new(
-        File::open(ldrawpath.join("LDConfig.ldr")).expect("Could not load color definition.")
-    )).expect("Could not parse color definition");
+        File::open(ldrawpath.join("LDConfig.ldr")).await.expect("Could not load color definition.")
+    )).await.expect("Could not parse color definition");
+
+    let loader: Box<dyn LibraryLoader> = Box::new(LocalLoader::new(Some(ldrawpath), None));
+
+    let mut tasks = vec![];
 
     let cache = Arc::new(RwLock::new(PartCache::default()));
     if let Some(files) = matches.values_of("files") {
         for file in files {
-            let path = Path::new(&file);
-            if !path.exists() {
+            let path = PathBuf::from(&file);
+            if !path.exists().await {
                 panic!("Path {} does not exists.", file);
-            } else if path.is_dir() {
-                for entry in path.read_dir().expect("Could not read directory.") {
+            } else if path.is_dir().await {
+                let mut dir = path.read_dir().await.expect("Could not read directory.");
+                while let Some(entry) = dir.next().await {
                     let entry = entry.unwrap();
                     let path = entry.path();
                     let ext = path.extension();
@@ -93,44 +104,66 @@ fn main() {
                     }
                     let ext = ext.unwrap().to_str().unwrap().to_string().to_lowercase();
                     if ext == "dat" || ext == "ldr" {
-                        bake(&colors, Arc::clone(&directory), Arc::clone(&cache), &path, &output_path)
+                        tasks.push(bake(&loader, &colors, Arc::clone(&cache), path, &output_path));
                     }
                 }
             } else {
-                bake(&colors, Arc::clone(&directory), Arc::clone(&cache), &path, &output_path);
+                tasks.push(bake(&loader, &colors, Arc::clone(&cache), path, &output_path));
             }
         }
     } else {
         panic!("Required input files are missing.");
     }
 
+    let cpus = num_cpus::get();
+    let tasks = tasks.into_iter().chunks(cpus).into_iter().map(|chunk| chunk.collect()).collect::<Vec<Vec<_>>>();
+    for items in tasks {
+        join_all(items).await;
+    }
+
     let collected = cache.write().unwrap().collect(CacheCollectionStrategy::PartsAndPrimitives);
     println!("Collected {} entries.", collected);
 }
 
-fn bake(colors: &MaterialRegistry,
-        directory: Arc<RwLock<PartDirectoryNative>>, cache: Arc<RwLock<PartCache>>, path: &Path,
+async fn bake(
+        loader: &Box<dyn LibraryLoader>,
+        colors: &MaterialRegistry,
+        cache: Arc<RwLock<PartCache>>,
+        path: PathBuf,
         output_path: &Option<&Path>) {
     println!("{}", path.to_str().unwrap());
 
-    let document = parse_multipart_document(&colors, &mut BufReader::new(
-        File::open(path).expect(&format!("Could not open document {}", path.to_str().unwrap()))
-    )).expect(&format!("Could not parse document {}", path.to_str().unwrap()));
-
-    let mut resolution = ResolutionMap::new(Arc::clone(&directory), Arc::clone(&cache));
-    resolution.resolve(&&document.body, Some(&document));
-    loop {
-        let files = match load_files(&colors, Arc::clone(&cache), resolution.get_pending()) {
-            Some(e) => e,
-            None => break,
-        };
-        for key in files {
-            let doc = cache.read().unwrap().query(&key).unwrap();
-            resolution.update(&key, doc);
+    let file = match File::open(path.clone()).await {
+        Ok(v) => v,
+        Err(err) => {
+            println!("Could not open document {}: {}", path.to_str().unwrap(), err);
+            return;
         }
-    }
+    };
 
-    let part = bake_part(&resolution, None, &document.body);
+    let document = match parse_multipart_document(&colors, &mut BufReader::new(&file)).await {
+        Ok(v) => v,
+        Err(err) => {
+            println!("Could not parse document {}: {}", path.to_str().unwrap(), err);
+            return;
+        }
+    };   
+
+    let resolution_result = resolve_dependencies(
+        Arc::clone(&cache),
+        &colors,
+        loader,
+        &document,
+        &|alias, result| {
+            if let Err(err) = result {
+                println!("Could not open file {}: {}", alias, err);
+            }
+        }
+    ).await;
+
+    let part = spawn_blocking(move || {
+        bake_part(&resolution_result, None, &document, false)
+    }).await.unwrap();
 
     let outpath = match output_path {
         Some(e) => {
@@ -147,11 +180,23 @@ fn bake(colors: &MaterialRegistry,
         }
     };
 
-    let _ = serialize_into(&mut BufWriter::new(File::create(&outpath).expect(
-        &format!("Could not create {}", outpath.to_str().unwrap())
-    )), &part);
+    match serialize(&part) {
+        Ok(serialized) => {
+            match File::create(&outpath).await {
+                Ok(file) => {
+                    let mut writer = BufWriter::new(file);
+                    writer.write_all(&serialized).await.unwrap();
+                    writer.close().await.unwrap();
+                },
+                Err(err) => {
+                    format!("Could not create {}", outpath.to_str().unwrap());
+                }
+            }
+        },
+        Err(err) => {
+            println!("Could not bake part {}: {}", path.to_str().unwrap(), err);
+        }
+    };
 
-    drop(resolution);
-    drop(document);
     cache.write().unwrap().collect(CacheCollectionStrategy::Parts);
 }
